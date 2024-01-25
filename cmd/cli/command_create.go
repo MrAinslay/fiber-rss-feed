2ad80683033eafb1a5e638c14a437036648f6f9c
MrAinslay <asteriospaligiannis@gmail.com>
2024-01-25 19:40:17 +0200

    Update command_create.go

--- cmd/cli/command_create.go
+++ cmd/cli/command_create.go
@@ -9,6 +9,10 @@ import (
 func commandCreate(cfg *ApiConfig, s string) error {
 	splitString := strings.Split(s, " ")
 	if splitString[0] == "user" {
+		if len(splitString) != 3 {
+
+		}
+
 		jsonBody := []byte(fmt.Sprintf(`{"name": "%s", "password": "%s"}`, splitString[1], splitString[2]))
 		bodyReader := bytes.NewReader(jsonBody)
 		cfg.ApiClient.HttpClient.Post(cfg.ApiClient.BaseURL, "application/json", bodyReader)
